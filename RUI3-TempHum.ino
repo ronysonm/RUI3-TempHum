8f5d2da6e602cebcc9f28a9cc72ec9c1bd43ce1f
Ronyson Martins <ronysonm@gmail.com>
2023-12-29 09:12:02 -0300

    Remove Led ATC and add SENDINT to control Send interval

--- RUI3-TempHum.ino
+++ RUI3-TempHum.ino
@@ -4,24 +4,17 @@
  *************************************/
 
 #include "rak1901.h"
-//#include "rak1902.h" 
 
 void uplink_routine();
 
-#define SMART_FARM_PERIOD (30000)
-
-// Define channel mask
-uint16_t maskBuff = 0x0002;
-
-// Led status
-#define LED_OFFSET 0x00000002 //lenght 4 bytes
-uint8_t led_status;
+// Send uplink interval
+#define SEND_INTERVAL_OFFSET 0x00000002
+#define DEFAULT_SEND_INTERVAL (60)                  // Default value if data in flash is empty or corrupted
+#define SEND_INTERVAL (30)                          // Hardcoded value, but overwrited by value in flash
+uint32_t g_send_interval = SEND_INTERVAL * 1000;
 
 /** Temperature & Humidity sensor **/
 rak1901 th_sensor;
-
-/** Air Pressure sensor **/
-//rak1902 p_sensor;
 
 /** Packet buffer for sending */
 uint8_t collected_data[64] = { 0 };
@@ -51,109 +44,123 @@ void sendCallback(int32_t status)
   }
 }
 
-int led_handle(SERIAL_PORT port, char*cmd, stParam *param)
+int send_interval_handler(SERIAL_PORT port, char *cmd, stParam *param)
 {
-  if (param->argc == 1 && !strcmp(param->argv[0], "?")) {
+  // Verify if arg is not null and the argument letter is ?
+  if(param->argc == 1 && !strcmp(param->argv[0], "?"))
+  {
     Serial.print(cmd);
     Serial.print("=");
-    Serial.println(led_status?"HIGH":"LOW");
-  } else if (param->argc == 1) {
-    for (int i = 0; i < strlen(param->argv[0]); i++) {
-      if (!isDigit(*(param->argv[0] +i))) {
+    Serial.printf("%ld\r\n", g_send_interval /1000);   
+  }
+  else if (param->argc == 1)
+  {
+    // MYLOG("AT_CMD", "param->[0] >> %s", param->argv[0]);
+    for (int i = 0; i < strlen(param->argv[0]); i++)
+    {
+      // Verify if each digit is a number
+      if(!isDigit(*(param->argv[0]+ i)))
+      {
+        // MYLOG("AT_CMD", " %d is no digit", i);
         return AT_PARAM_ERROR;
       }
     }
+    // Convert string to int
+    uint32_t new_send_interval = strtoul(param->argv[0], NULL, 10);
 
-    led_status = strtoul(param->argv[0], NULL, 10);
-    if (led_status != 0 && led_status != 1) {
-      return AT_PARAM_ERROR;
+    // MYLOG(("AT_CMD", "Requested interval %ld", new_send_interval);
+
+    g_send_interval = new_send_interval * 1000;
+
+    // MYLOG("AT_CMD", "New interval %ld", g_send_interval);
+
+    // Stop the timer
+    api.system.timer.stop(RAK_TIMER_0);
+    // Start new timer if not disabled (0)
+    if (g_send_interval != 0)
+    {
+      // Restart the timer
+      api.system.timer.start(RAK_TIMER_0, g_send_interval, NULL);
     }
-
-    pinMode(GREEN_LED, OUTPUT);
-    pinMode(BLUE_LED, OUTPUT);
-    digitalWrite(GREEN_LED, (led_status == 1) ? HIGH : LOW);
-    digitalWrite(BLUE_LED, (led_status == 1) ? HIGH : LOW);
+    // Save custom settings
     save_at_setting(true);
-
-  } else {
+    return AT_OK;
+  }
+  else 
+  {
     return AT_PARAM_ERROR;
   }
-
   return AT_OK;
 }
 
 bool get_at_setting(bool get_timeout)
 {
   uint8_t flash_value[16];
-  uint32_t offset = LED_OFFSET;
+  uint32_t sendint_offset = SEND_INTERVAL_OFFSET;
 
-  if (!api.system.flash.get(offset, flash_value, 5))
+  if (!api.system.flash.get(sendint_offset, flash_value, 5))
   {
     Serial.println("Get values from flash fail!");
     return false;
   }
 
-  // If read invalid data from flash, will set defaults values 
-  if (flash_value[4] != 0xAA)
+  // If read invalid data from flash, will set defaults values
+  // SEND_INTERVAL_OFFSET 4 bytes
+
+  if (flash_value[4] != 0XAA)
   {
-    Serial.printf("AT_CMD", "No valid LED Status found, read 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X \r\n",
-                              flash_value[0], flash_value[1], flash_value[2], flash_value[3], flash_value[4]);
-    if (led_status)
-    {
-      led_status = 0;
-    }
+    Serial.printf("AT_CMD", " No valid SENDINT found, read 0x%02X 0x%02X 0x%02X 0x%02X 0x%02X from OFFSET %d\r\n",
+                              flash_value[0], flash_value[1], flash_value[2], flash_value[3], flash_value[4], sendint_offset);
     save_at_setting(true);
     return false;
   }
-  Serial.printf("Read Led status from flash 0x%02X 0x%02X 0x%02X 0x%02X 0X%02X \r\n",
-                                  flash_value[0], flash_value[1], 
-                                  flash_value[2], flash_value[3], flash_value[4]);
+  Serial.printf("Read SENDINT from flash 0x%02X 0x%02X 0x%02X 0x%02X \r\n",
+                                  flash_value[0], flash_value[1], flash_value[2], flash_value[3]);
   if (get_timeout)
   {
-    led_status = 0;
-    led_status |= flash_value[0] << 0;
-    led_status |= flash_value[1] << 8;
-    led_status |= flash_value[2] << 16;
-    led_status |= flash_value[3] << 24;
-    pinMode(GREEN_LED, OUTPUT);
-    pinMode(BLUE_LED, OUTPUT);
-    digitalWrite(GREEN_LED, (led_status == 1) ? HIGH : LOW);
-    digitalWrite(BLUE_LED, (led_status == 1) ? HIGH : LOW);
-    Serial.printf("AT_CMD", " Led status found %d\r\n", led_status);
+    g_send_interval = 0;
+    g_send_interval |= flash_value[0] << 0;
+    g_send_interval |= flash_value[1] << 8;
+    g_send_interval |= flash_value[2] << 16;
+    g_send_interval |= flash_value[3] << 24;
   }
-  Serial.printf("LED Status: %d\r\n", led_status);
+  Serial.printf("SENDINT: %d\r\n", g_send_interval / 1000);
   return true;
 }
 
-bool save_at_setting(bool set_led_status)
+bool save_at_setting(bool save_settings)
 {
   uint8_t flash_value[16] = {0};
-  bool wr_result = false;
-  uint32_t offset = LED_OFFSET;
-  if (set_led_status)
+  bool write_result = false;
+  uint32_t sendint_offset = SEND_INTERVAL_OFFSET;
+  
+  if (save_settings)
   {
-    offset = LED_OFFSET;
-    flash_value[0] = (uint8_t)(led_status >> 0);
-    flash_value[1] = (uint8_t)(led_status >> 8);
-    flash_value[2] = (uint8_t)(led_status >> 16);
-    flash_value[3] = (uint8_t)(led_status >> 24);
+    flash_value[0] = (uint8_t)(g_send_interval >> 0);
+    flash_value[1] = (uint8_t)(g_send_interval >> 8);
+    flash_value[2] = (uint8_t)(g_send_interval >> 16);
+    flash_value[3] = (uint8_t)(g_send_interval >> 24);
     flash_value[4] = 0xAA;
   }
-  Serial.printf("AT_CMD", " Writing time 0X%02 0X%02 0X%02 0X%02 to %d",
+  Serial.printf("AT_CMD", " Writing time 0x%02X 0x%02X 0x%02X 0x%02X to %d\r\n",
                     flash_value[0], flash_value[0], 
-                    flash_value[0], flash_value[0], offset);
-  wr_result = api.system.flash.set(offset, flash_value, 5);
-  if (!wr_result) 
+                    flash_value[0], flash_value[0], sendint_offset);
+  write_result = api.system.flash.set(sendint_offset, flash_value, 5);
+
+  if (!write_result) 
   {
     // Retry
-    wr_result = api.system.flash.set(offset, flash_value, 5);
+    write_result = api.system.flash.set(sendint_offset, flash_value, 5);
+    Serial.printf("Retrying Writing on flash\r\n");
   }
-  wr_result = true;
-  return wr_result;
+  Serial.printf("Writing on flash\r\n");
+  write_result = true;
+  return write_result;
 }
 
 void setup() 
 {
+  // Convert seconds to miliseconds
   Serial.begin(115200, RAK_AT_MODE);
   delay(2000);
 
@@ -162,34 +169,35 @@ void setup()
 
   // Get saved led status
   get_at_setting(true);
+  
+  // Set custom ATC Commands
+  api.system.atMode.add("SENDINT", "Get/Set interval sending time in seconds", "SENDINT", send_interval_handler, RAK_ATCMD_PERM_WRITE | RAK_ATCMD_PERM_READ);
 
   if (!api.lorawan.join()) {
     Serial.printf("LoraWan Smart Farm - join fail! \r\n");
   }
 
-  Serial.println("++++++++++++++++++++++++++");
-  Serial.println("RUI3 Environment Sensing");
-  Serial.println("++++++++++++++++++++++++++");
-  
   api.lorawan.registerRecvCallback(recvCallback);
   api.lorawan.registerJoinCallback(joinCallback);
   api.lorawan.registerSendCallback(sendCallback);
 
-  Wire.begin(); // Start I2C Bus
-  Serial.printf("RAK1901 init %s\r\n", th_sensor.init() ? "success" : "fail"); // Check if RAK1901 init success
+  // Start I2C Bus
+  Wire.begin(); 
+  // Check if RAK1901 init success
+  Serial.printf("RAK1901 init %s\r\n", th_sensor.init() ? "success" : "fail"); 
 
-  if (api.system.timer.create(RAK_TIMER_0, (RAK_TIMER_HANDLER)uplink_routine, RAK_TIMER_PERIODIC) != true) {
+  if (!api.system.timer.create(RAK_TIMER_0, (RAK_TIMER_HANDLER) uplink_routine, RAK_TIMER_PERIODIC)) {
     Serial.printf("Lorawan Smart Farm - Creating timer failed! \r\n");
     return;
   }
 
-  if (api.system.timer.start(RAK_TIMER_0, SMART_FARM_PERIOD, NULL) != true) {
-    Serial.printf("Lorawan Smart Farm - Starting timer failed! \r\n");
-    return;
+  if (g_send_interval != 0) 
+  {
+    if (!api.system.timer.start(RAK_TIMER_0, g_send_interval, NULL)) {
+      Serial.printf("Lorawan Smart Farm - Starting timer failed! \r\n");
+      return;
+    }
   }
-
-  api.system.atMode.add("LED", "This controls both green and blue LEDS.", "LED", led_handle, RAK_ATCMD_PERM_WRITE | RAK_ATCMD_PERM_READ);
-
 }
 
 void uplink_routine()
@@ -240,6 +248,7 @@ void loop() {
   /* 
   *  Destroy this busy loop and use timer to do what you want instead,
   *  so that the system thread can auto enter low power mode by api.system.lpm.set(1); 
+  *  https://github.com/beegee-tokyo/RUI3-LowPower-Example?tab=readme-ov-file#setup-and-loop
   */
   
   api.system.scheduler.task.destroy();
